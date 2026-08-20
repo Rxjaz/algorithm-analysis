50fd853ab5d139554b432c2a0955a43d80ad7aab
Rxjaz <ariel.rxj@gmail.com>
2026-08-19 20:27:05 -0600

    First Class (if, for, bubble)

---
+++ 02_for.py
@@ -0,0 +1,12 @@
+n=[1, 2 ,3, 4, 5, 6, 7]
+
+print("Primer for", "\n")
+for i in n:
+    print(i)
+
+print("--------------------------", "\n")
+print("Segundo for", "\n")
+for i in n:
+    print(i)
+
+print("--------------------------", "\n")
